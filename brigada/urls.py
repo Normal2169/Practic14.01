064eb44b8a801d129bb046b4da7736e83ad8d993
Normal2169 <vladimerp744@gmail.com>
2025-11-12 14:38:51 +0500

    theree django

--- brigada/urls.py
+++ brigada/urls.py
@@ -2,7 +2,11 @@ from django.urls import path, re_path
 from hello import views
  
 urlpatterns = [
-    path('', views.index),
-    re_path('^about', views.about, kwargs={"name":"Tom", "age": 38}),
-    re_path('contact', views.contact),
+    path('', views.defolt),
+    path('index', views.index),
+    path('about', views.about, kwargs={"name":"Tom", "age": 38}),
+    path('contact', views.contact),
+    path("user", views.user),
+    path("user/<name>", views.user),
+    path("user/<name>/<int:age>", views.user),
 ]
--- hello/views.py
+++ hello/views.py
@@ -1,6 +1,5 @@
 from django.http import HttpResponse
-  
-def index(request):
+def defolt(request):
     host = request.META["HTTP_HOST"]
     user_agent = request.META["HTTP_USER_AGENT"]
     path = request.path
@@ -9,13 +8,22 @@ def index(request):
         <p>User-agnet: {user_agent}</p>
         <p>Path: {path}</p>
                         """)
-  
+def index(request):
+    host = request.META["HTTP_HOST"]
+    user_agent = request.META["HTTP_USER_AGENT"]
+    path = request.path
+    return HttpResponse(f"""
+        <p>Host: {host}</p>
+        <p>User-agnet: {user_agent}</p>
+        <p>Path: {path}</p>
+            """)
 def about(request,name,age):
     return HttpResponse(f"""
             <h2>О пользователе</h2>
             <p>Имя: {name}</p>
             <p>Возраст: {age}</p>
-                        """)
-  
+             """, headers={"SecretCode": age})
+def user(request, name="Undefined", age =0):
+    return HttpResponse(f"<h2>Имя: {name}  Возраст: {age}</h2>")
 def contact(request):
     return HttpResponse("<h2>Контакты</h2>")
