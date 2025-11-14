af3cad9b568483ecfc333e1e7529c1f7d9facf06
Normal2169 <vladimerp744@gmail.com>
2025-11-14 15:27:14 +0500

    four django

--- brigada/urls.py
+++ brigada/urls.py
@@ -1,12 +1,6 @@
-from django.urls import path, re_path
+from django.urls import path
 from hello import views
- 
+  
 urlpatterns = [
-    path('', views.defolt),
-    path('index', views.index),
-    path('about', views.about, kwargs={"name":"Tom", "age": 38}),
-    path('contact', views.contact),
-    path("user", views.user),
-    path("user/<name>", views.user),
-    path("user/<name>/<int:age>", views.user),
+    path("set", views.set),
 ]
--- hello/views.py
+++ hello/views.py
@@ -1,29 +1,11 @@
 from django.http import HttpResponse
-def defolt(request):
-    host = request.META["HTTP_HOST"]
-    user_agent = request.META["HTTP_USER_AGENT"]
-    path = request.path
-    return HttpResponse(f"""
-        <p>Host: {host}</p>
-        <p>User-agnet: {user_agent}</p>
-        <p>Path: {path}</p>
-                        """)
-def index(request):
-    host = request.META["HTTP_HOST"]
-    user_agent = request.META["HTTP_USER_AGENT"]
-    path = request.path
-    return HttpResponse(f"""
-        <p>Host: {host}</p>
-        <p>User-agnet: {user_agent}</p>
-        <p>Path: {path}</p>
-            """)
-def about(request,name,age):
-    return HttpResponse(f"""
-            <h2>О пользователе</h2>
-            <p>Имя: {name}</p>
-            <p>Возраст: {age}</p>
-             """, headers={"SecretCode": age})
-def user(request, name="Undefined", age =0):
-    return HttpResponse(f"<h2>Имя: {name}  Возраст: {age}</h2>")
-def contact(request):
-    return HttpResponse("<h2>Контакты</h2>")
+ 
+# установка куки
+def set(request):
+    # получаем из строки запроса имя пользователя
+    username = request.GET.get("username", "Undefined")
+    # создаем объект ответа
+    response = HttpResponse(f"Hello {username}")
+    # передаем его в куки
+    response.set_cookie("username", username)
+    return response
